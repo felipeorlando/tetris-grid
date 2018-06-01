8a5ec68e39248d1cc251accd0a3c1b4c2c26deaf
Felipe Orlando <fobsouza@gmail.com>
2018-06-01 16:49:07 -0300

    Create basic grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
 const gulp = require('gulp')
+const path = require('path')
 const runSequence = require('run-sequence')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const cleanCSS = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
 
+const SASS_INCLUDE_PATHS = [
+  path.join(__dirname, 'node_modules', 'bourbon-neat', 'core'),
+]
+
 gulp.task('styles', () => {
   return gulp.src('./src/tetris.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ includePaths: SASS_INCLUDE_PATHS }).on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('minify-styles', () => {
-  return gulp.src('./dist/tetris.css')
+  return gulp.src('./src/tetris.scss')
+    .pipe(sass({ includePaths: SASS_INCLUDE_PATHS }).on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
     .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'))
 })
 
